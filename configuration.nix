9d9ec9d470186e4a4d13c71d2247f473846d5bd7
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2016-02-11 20:39:11 +0100

    Provide vim with a vimrc file.

--- configuration.nix
+++ configuration.nix
@@ -2,6 +2,11 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
+# This configuration file simply determines the hostname before importing most
+# of the actual configuration from ./configuration-common.nix. This is done so
+# that the same configuration files can be used with both conventionally
+# installed NixOS (see nixos-install) and NixOS installed by Nixops.
+
 { config, pkgs, ... }:
 
 {
@@ -13,13 +18,13 @@
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/sda";
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.firewall = {
     enable = true;
+    allowPing = true;
     allowedTCPPorts = [ 22 ];
   };
 
@@ -33,27 +38,36 @@
   # Set your time zone.
   time.timeZone = "Europe/Amsterdam";
 
+  # Change the default config of some packages.
+  security.sudo.wheelNeedsPassword = false;
+
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
     git
     tmux
-    (vim_configurable.customize {
-      name = "vim";
-      vimrcConfig.customRC = (import ./vimrc.nix {});
-    })
+    vim
   ];
 
-  # Change the default config of some packages.
-  security.sudo.wheelNeedsPassword = false;
+  nixpkgs.config.packageOverrides = pkgs: {
+    vim = pkgs.stdenv.lib.overrideDerivation (
+      pkgs.vim_configurable.customize {
+        name = "vim";
+        vimrcConfig.customRC = (import ./vimrc.nix {});
+      }
+    ) (o: {
+      postInstall = "ln -s vim $out/bin/vi";
+    });
+  };
+
+  # Change some program configurations.
+  programs.bash.enableCompletion = true;
 
   # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
   # Enable the X11 windowing system.
-  # services.xserver.enable = true;
+  services.xserver.enable = true;
   # services.xserver.layout = "us";
   # services.xserver.xkbOptions = "eurosign:e";
 
@@ -70,5 +84,4 @@
 
   # The NixOS release to be compatible with for stateful data such as databases.
   system.stateVersion = "15.09";
-
 }
